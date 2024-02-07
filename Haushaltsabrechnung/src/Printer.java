6b1f9ae71b20d90ece9f198154af20aee52b8ca9
Luis Maier <luis.maier@sap.com>
2024-02-07 10:17:13 +0100

    printer überarbeitung

--- Haushaltsabrechnung/src/Printer.java
+++ Haushaltsabrechnung/src/Printer.java
@@ -9,18 +9,22 @@ public class Printer {
     }
 
     public void printAbrechnung (Haushaltsabrechnung abrechnung) {
-        System.out.println("<---Start----Haushaltsabrechnung---->");
-        System.out.println("I | Objektname | Monate | Gezahlt | Angefallen | Restbetrag |");
         //for loop durch H durch - dann pro eintrag einmal alles in einen String und output!
         Set<Entry<Integer, Eintrag>> entrySet = abrechnung.get_Abrechnung().entrySet();
         if (entrySet.size() == 0) {
             System.out.println("Haushaltsabrechnung leer!");
         } else {
+            System.out.printf("|------------------------Haushaltsabrechnung------------------------|%n");
+            System.out.printf("| %-20s | %-6s | %7s | %10s | %10s |%n", "Objektname", "Monate", "Gezahlt", "Angefallen", "Restbetrag");
+            System.out.printf("|-------------------------------------------------------------------|%n");
             for (Map.Entry<Integer, Eintrag> entry : entrySet) {
                 Eintrag eintrag = entry.getValue();
-                System.out.println(entry.getKey() +" | " + eintrag.getObjektname() + " | " + eintrag.getGueltigeMonate() + " | " + eintrag.getGezahlterBetrag() + " | " + eintrag.getTatsaechlicherBetrag() + " | " + eintrag.getRestbetrag()+ " |");
+                //System.out.println(entry.getKey() +" | " + eintrag.getObjektname() + " | " + eintrag.getGueltigeMonate() + " | " + eintrag.getGezahlterBetrag() + " | " + eintrag.getTatsaechlicherBetrag() + " | " + eintrag.getRestbetrag()+ " |");
+                System.out.printf("| %-20s | %-6s | %7s | %10s | %10s |%n", eintrag.getObjektname(), eintrag.getGueltigeMonate(),  eintrag.getGezahlterBetrag(), eintrag.getTatsaechlicherBetrag(), eintrag.getRestbetrag());
             }
+            System.out.printf("|-------------------------------------------------------------------|%n");
+            System.out.printf("| %-20s | %-6s | %7s | %10s | %10s |%n", "Gesamtbeträge", "------",  "demo", "demo", "demo");
+            System.out.printf("|-------------------------------------------------------------------|%n");
         }
-        System.out.println("<----Ende----Haushaltsabrechnung---->");
     }
 }
